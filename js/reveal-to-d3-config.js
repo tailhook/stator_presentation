de7392e7c56c6917dbff8e69f915238145c539f5
Paul Colomiets <paul@colomiets.name>
2016-04-22 02:19:57 +0300

    Graphs are almost final

--- js/reveal-to-d3-config.js
+++ js/reveal-to-d3-config.js
@@ -30,7 +30,7 @@ pt.slideIdToFunctions = {
     },
     2: function() {
       'use strict';
-      // Hello world + async things
+      // Redis
       var data = {
         stator: 16439.26,
         "stator-aio-redis": 17200.96,
@@ -39,6 +39,30 @@ pt.slideIdToFunctions = {
         "uwsgi-gevent": 6782.57,
       };
       render(data);
-    }
+    },
+    3: function() {
+      'use strict';
+      // Redis + 10 ms sleep
+      var data = {
+        stator: 1753.80,
+        "stator-aio-redis": 14825.91,
+        "stator-redis": 12759.67,
+        uwsgi: 1665.79,
+        "uwsgi-gevent": 6967.74,
+      };
+      render(data);
+    },
+    4: function() {
+      'use strict';
+      // Redis + 10 ms busy
+      var data = {
+        stator: 1769.15,
+        "stator-aio-redis": 912.20,
+        "stator-redis": 931.20,
+        uwsgi: 1748.15,
+        "uwsgi-gevent": 1482.26,
+      };
+      render(data);
+    },
   },
 };
